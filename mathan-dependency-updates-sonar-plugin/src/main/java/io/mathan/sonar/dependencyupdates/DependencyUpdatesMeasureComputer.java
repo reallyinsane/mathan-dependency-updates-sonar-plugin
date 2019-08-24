6a844ab2d0526d6779e5b8ed6db1da7eade550cf
pether.sorling@gmail.com <pether.sorling@gmail.com>
2019-08-25 00:56:53 +0200

    fix build

--- mathan-dependency-updates-sonar-plugin/src/main/java/io/mathan/sonar/dependencyupdates/DependencyUpdatesMeasureComputer.java
+++ mathan-dependency-updates-sonar-plugin/src/main/java/io/mathan/sonar/dependencyupdates/DependencyUpdatesMeasureComputer.java
@@ -74,7 +74,7 @@ public class DependencyUpdatesMeasureComputer implements MeasureComputer {
       ratio = 100 * count / total;
     }
     context.addMeasure(ratioMetric, ratio);
-    context.addMeasure(ratingMetric, Metrics.calculateRating(ratio, total));
+    context.addMeasure(ratingMetric, Metrics.calculateRating(count, total));
   }
 
   private void maxMeasure(MeasureComputerContext context, String metricKey) {
